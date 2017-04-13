44176a3514e2d1b36fec5f075df9c28c3c2769eb
A Wan <alewan@users.noreply.github.com>
2017-04-13 01:23:01 -0400

    Added scorekeeping (output to 7seg HEX display),  and master game start (KEY0)

--- project.s
+++ project.s
@@ -4,8 +4,9 @@
 #"Global Variables"
 #r22 is X location of center of the ball, r20 is X directionality
 #r23 is Y location of center of the ball, r21 is Y directionality
-#r16 is previous top of the left bar, r18 is the new top of the left bar
-#r17 is previous top of the right bar, r19 is the new top of the right bar
+#r18 is the new top of the left bar
+#r19 is the new top of the right bar
+#r16 (Player 1) and r17 (Player 2) are the scores
 
 #External Devices
 .equ JTAG_UART, 0xFF201000
@@ -16,10 +17,11 @@
 .equ VGA_CHAR_BASE, 0x09000000
 .equ HEX_DISPLAY, 0xFF200020 #every 8 bits is a new HEX display until bit 30 (6-0 H0, 14-8 H1, 22-16 H2, 30-24 H3)
 .equ AUDIO, 0XFF203040
-.equ IRQ_SETUP, 0x00000001
+.equ IRQ_SETUP, 0x00000003 #line0 for timer, line1 for keys
 .equ TIMER1, 0xFF202000
 .equ ADDR_JP2PORT, 0xFF200070
 .equ ADDR_JP2PORT_DIR, 0xFF200074
+.equ KEYS, 0xFF200050
 
 #VGA Definitions
 .equ XMAX, 320
@@ -30,7 +32,7 @@
 .equ BAR2_START_X, 290
 .equ BAR1_START_Y, 114
 .equ BAR2_START_Y, 114
-.equ BAR_SIZE, 50
+.equ BAR_SIZE, 25
 .equ WHITE, 0xFFFF
 .equ GREY, 0x8410
 .equ BLACK, 0x0000
@@ -38,6 +40,19 @@
 .equ GREEN, 0x07E0
 .equ BLUE, 0x001F
 .equ PURPLE, 0xF81F
+
+#Hex Definitions
+.equ HEX_DISPLAY_VAL0, 0x3F
+.equ HEX_DISPLAY_VAL1, 0x06
+.equ HEX_DISPLAY_VAL2, 0x5B
+.equ HEX_DISPLAY_VAL3, 0x4F
+.equ HEX_DISPLAY_VAL4, 0x66
+.equ HEX_DISPLAY_VAL5, 0x6D
+.equ HEX_DISPLAY_VAL6, 0x6D
+.equ HEX_DISPLAY_VAL7, 0x7D
+.equ HEX_DISPLAY_VAL8, 0x07
+.equ HEX_DISPLAY_VAL9, 0x7F
+.equ HEX_DISPLAY_VALA, 0x6F
 
 
 #TO SAVE IN THE ISR
@@ -52,19 +67,40 @@
 .section .exceptions,"ax"
 ISR:
 	rdctl et, ipending #check ipending
-	#Jumping if not timer interrupt
+	#look for timer interrupt
 	andi et, et, 0x1
+	bne et, r0, TIMER_ISR
+	rdctl et, ipending
+	#look for key interrupt
+	andi et, et, 0x2
 	beq et, r0, END_ISR
 
+KEYS_ISR:
+	#Start of keys interrupt handling
+	#acknowledge interrupt
+	movia et, KEYS
+	stwio r0, 12(et)
+	#prevent future keys interrupts
+	stwio r0, 8(et)
+	#Exit to break the loop
+	eret
+	#End of keys interrupt handling
+
+TIMER_ISR:
 	#Start of Timer Interrupt Handling
-	addi sp, sp, -28
+	addi sp, sp, -40
 	stw r8,(sp)
 	stw r9,4(sp)
 	stw r10,8(sp)
 	stw r11,12(sp)
 	stw r12,16(sp)
 	stw r13,20(sp)
-	stw r14,20(sp)
+	stw r14,24(sp)
+	stw r4,28(sp)
+	stw r5,32(sp)
+	stw r6,36(sp)
+	
+
 	#check the fsr
 	movia r11, ADDR_JP2PORT_DIR
 	stwio r0, 0(r11)
@@ -76,6 +112,7 @@ ISR:
 	retFromFSR:
 	call REDRAW_ICON
 	call REDRAW_BAR
+	call UPDATE_SCORE
 	#acknowledge interrupt and reset timer
 	movia et, TIMER1
 	stwio r0, (et)
@@ -88,7 +125,10 @@ ISR:
 	ldw r12,16(sp)
 	ldw r13,20(sp)
 	ldw r14,24(sp)
-	addi sp, sp, 28
+	ldw r4,28(sp)
+	ldw r5,32(sp)
+	ldw r6,36(sp)
+	addi sp, sp, 40
 	br END_ISR
 	#End of Timer Interrupt Handling
 
@@ -105,6 +145,10 @@ movia sp, 0x04000000
 
 movui r4, BLACK
 call RESET_VGA
+
+#Initialize scores to zero
+mov r16, r0
+mov r17, r0
 
 #Draw first bar
 movui r4, WHITE
@@ -130,9 +174,8 @@ movui r23, BALL_START_Y
 addi r20,r0,1
 addi r21,r0,1
 
+call UPDATE_SCORE
 #END SETUP
-
-
 
 #ISR_SETUP
 	#Enable interrups
@@ -140,6 +183,15 @@ addi r21,r0,1
 	wrctl ienable, r9 #ienable = ctl3
 	movi r9, 0b1
 	wrctl status, r9 #Enable PIE
+
+	#Setup keys and then wait for keypress to start the game
+	movia r8, KEYS
+	movia r9,0x1
+	stwio r9,8(r8)
+	stwio r0,12(r8)
+	#MESSGAE TO VGA BEFORE START OF GAME
+	WAIT_FOR_KEYPRESS: br WAIT_FOR_KEYPRESS
+
 
 	#Initialize counter value
 	movia r8, TIMER1
@@ -152,9 +204,6 @@ addi r21,r0,1
 	movui r9, 0b101 #Enable start, no CTS, enable interrupt
 	stwio r9, 4(r8)
 #END ISR_SETUP
-#initialize displacement of left bar
-movi r16, 0
-movi r18, 0
 
 #number of samples needed
 movi r8, 384
@@ -224,7 +273,6 @@ br returnFromLocalAmp
 
 updateMaxAmp:
 movi r10, 0
-mov r16, r18
 #temp register for shifting
 mov r13, r9
 srli r13, r13, 23
@@ -336,11 +384,8 @@ DRAW_BAR_SECTION:
 #Function to redraw the bar
 REDRAW_BAR:
 	#Prologue
-	addi sp, sp, -16
+	addi sp, sp, -4
 	stw ra,(sp)
-	stw r4,4(sp)
-	stw r5,8(sp)
-	stw r6,12(sp)
 
 	#Initialization
 	movia r8, VGA_PIXEL_BASE
@@ -362,16 +407,9 @@ REDRAW_BAR:
 	mov r6, r19
 	call DRAW_BAR
 
-	#Update old and new
-	add r16,r0,r18
-	add r17,r0,r19
-
 	#Epliogue
 	ldw ra,(sp)
-	ldw r4,4(sp)
-	ldw r5,8(sp)
-	ldw r6,12(sp)
-	addi sp, sp, 16
+	addi sp, sp, 4
 	ret
 
 
@@ -412,11 +450,8 @@ DRAW_ICON:
 #Function to re-draw 3x3 icon at a location
 REDRAW_ICON:
 	#Prologue
-	addi sp, sp, -16
+	addi sp, sp, -4
 	stw ra,(sp)
-	stw r4,4(sp)
-	stw r5,8(sp)
-	stw r6,12(sp)
 
 	#Initialization
 	movia r8, VGA_PIXEL_BASE
@@ -465,13 +500,17 @@ DONE_CHECK_HIT_B1:
 	xori r20,r20,1
 	br CHECK_Y_DIR
 DONE_CHECK_HIT_B2:
-
 	#Check for the wall
 	movui r4,XMAX
 	#Check X border, Y border
-	bge r22,r4,CHANGE_X_DIR
-	ble r22,r0,CHANGE_X_DIR
+	bge r22,r4,CHANGE_X_DIR_GOALP1
+	ble r22,r0,CHANGE_X_DIR_GOALP2
 	br CHECK_Y_DIR
+CHANGE_X_DIR_GOALP1:
+	addi r16,r16,1
+	br CHANGE_X_DIR
+CHANGE_X_DIR_GOALP2:
+	addi r17,r17,1
 CHANGE_X_DIR:
 	xori r20,r20,1
 CHECK_Y_DIR:
@@ -491,21 +530,142 @@ END_CHANGE_DIR:
 	
 	#Epliogue
 	ldw ra,(sp)
-	ldw r4,4(sp)
-	ldw r5,8(sp)
-	ldw r6,12(sp)
-	addi sp, sp, 16
+	addi sp, sp, 4
 	ret
 
-#Function to write a value in r4 to the 7-seg display HEX0
-DRAW_HEX0_DIGIT:
+#Function to write a value in r16 to the 7-seg display HEX0 and r17 to HEX1
+UPDATE_SCORE:
 	#Prologue (N/A)
-	
+
 	#Initialization
 	movia r8, HEX_DISPLAY
 	
 	#Core
-	
-	
-	#Epilogue
+	#Check value in r16
+	beq r16, r0, DRAW0_P1
+	addi r9,r0,1
+	beq r16, r9, DRAW1_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW2_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW3_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW4_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW5_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW6_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW7_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW8_P1
+	addi r9,r9,1
+	beq r16, r9, DRAW9_P1
+	addi r9,r9,1
+	beq r16, r9, DRAWA_P1
+
+#Update the score
+DRAW0_P1:
+	movui r9, HEX_DISPLAY_VAL0
+	br UPDATE_P2
+DRAW1_P1:
+	movui r9, HEX_DISPLAY_VAL1
+	br UPDATE_P2
+DRAW2_P1:
+	movui r9, HEX_DISPLAY_VAL2
+	br UPDATE_P2
+DRAW3_P1:
+	movui r9, HEX_DISPLAY_VAL3
+	br UPDATE_P2
+DRAW4_P1:
+	movui r9, HEX_DISPLAY_VAL4
+	br UPDATE_P2
+DRAW5_P1:
+	movui r9, HEX_DISPLAY_VAL5
+	br UPDATE_P2
+DRAW6_P1:
+	movui r9, HEX_DISPLAY_VAL6
+	br UPDATE_P2
+DRAW7_P1:
+	movui r9, HEX_DISPLAY_VAL7
+	br UPDATE_P2
+DRAW8_P1:
+	movui r9, HEX_DISPLAY_VAL8
+	br UPDATE_P2
+DRAW9_P1:
+	movui r9, HEX_DISPLAY_VAL9
+	br UPDATE_P2
+DRAWA_P1:
+	movui r9, HEX_DISPLAY_VALA
+	stwio r9,(r8)
+	#GAMEOVER
+	movia ea, _start
+	eret
+
+UPDATE_P2:
+	stwio r9,(r8) #Sending P1 score to hex display
+	addi r8,r8,8
+#Check value in r17
+	beq r17, r0, DRAW0_P2
+	addi r9,r0,1
+	beq r17, r9, DRAW1_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW2_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW3_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW4_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW5_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW6_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW7_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW8_P2
+	addi r9,r9,1
+	beq r17, r9, DRAW9_P2
+	addi r9,r9,1
+	beq r17, r9, DRAWA_P2
+
+DRAW0_P2:
+	movui r9, HEX_DISPLAY_VAL0
+	br END_UPDATE_SCORE
+DRAW1_P2:
+	movui r9, HEX_DISPLAY_VAL1
+	br END_UPDATE_SCORE
+DRAW2_P2:
+	movui r9, HEX_DISPLAY_VAL2
+	br END_UPDATE_SCORE
+DRAW3_P2:
+	movui r9, HEX_DISPLAY_VAL3
+	br END_UPDATE_SCORE
+DRAW4_P2:
+	movui r9, HEX_DISPLAY_VAL4
+	br END_UPDATE_SCORE
+DRAW5_P2:
+	movui r9, HEX_DISPLAY_VAL5
+	br END_UPDATE_SCORE
+DRAW6_P2:
+	movui r9, HEX_DISPLAY_VAL6
+	br END_UPDATE_SCORE
+DRAW7_P2:
+	movui r9, HEX_DISPLAY_VAL7
+	br END_UPDATE_SCORE
+DRAW8_P2:
+	movui r9, HEX_DISPLAY_VAL8
+	br END_UPDATE_SCORE
+DRAW9_P2:
+	movui r9, HEX_DISPLAY_VAL9
+	br END_UPDATE_SCORE
+DRAWA_P2:
+	movui r9, HEX_DISPLAY_VALA
+	stwio r9,(r8)
+	#GAMEOVER
+	movia ea, _start
+	eret
+
+END_UPDATE_SCORE:
+	stwio r9,(r8) #Sending P2 score to hex display
+	#Epilogue (N/A)
 	ret
